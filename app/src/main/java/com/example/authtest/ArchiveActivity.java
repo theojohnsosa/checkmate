7850f61b29c403e2d189461534db8d3da9de39fe
Theojohn Sosa <theojohnsosa@gmail.com>
2026-01-19 12:54:01 +0800

    removed log statements in ArchiveActivity and ClassAdapter

--- app/src/main/java/com/example/authtest/ArchiveActivity.java
+++ app/src/main/java/com/example/authtest/ArchiveActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
