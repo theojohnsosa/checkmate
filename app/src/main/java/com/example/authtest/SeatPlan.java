fd88cada1d71b549b989f7c606b27d77d5652fdf
Andrie Hitomo <andriehitomo021@gmail.com>
2026-01-23 02:04:32 +0800

    implemented the view seatplan but not yet done in xml and cardview

--- app/src/main/java/com/example/authtest/SeatPlan.java
+++ app/src/main/java/com/example/authtest/SeatPlan.java
@@ -20,7 +20,7 @@ public class SeatPlan extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        //
 
     }
 }
